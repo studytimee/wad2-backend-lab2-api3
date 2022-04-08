2137fa12fbe4ff0290e4192c8076ed03c72ed3db
Abid Ali <studytimee@gmail.com>
2022-04-08 15:28:30 +0100

    User Accounts - Added Accounts Endpoints
    We will continue to develop an API for the MovieDB you completed in the last lab.
    
    In our API, we will manage user accounts and use account details to control access to the movie endpoints. The first part of the lab will create accounts endpoints.
    
    Exercise:
           1- Post some account data to the API
           2- Once you've added some Account data, do a GET /api/accounts and GET /api/accounts/:id to see if it worked!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './src/movies';
+import createAccountsRouter from './src/accounts/routes';
+import dependencies from './src/config/dependencies';
 
 dotenv.config();
 
@@ -9,7 +11,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use('/api/movies', moviesRouter);
+app.use('/api/accounts', createAccountsRouter(dependencies()));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
