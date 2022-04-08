8f9f0dd0a9558a8afd9a50ae25b9ada55bcce644
Abid Ali <studytimee@gmail.com>
2022-04-08 14:12:33 +0100

    Copied from wad2-backend-lab1-api2 into wad2-backend-lab2-api3
    In this lab you will:
    
    Create an API for managing users and movies.
    Use In-Memory Repository to persist data.
    Integrate the external TMDB API.

---
+++ index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import moviesRouter from './src/movies';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use('/api/movies', moviesRouter);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
