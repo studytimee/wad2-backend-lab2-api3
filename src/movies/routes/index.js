267b5e109aaf14c881d85c01fca2827711f64447
Abid Ali <studytimee@gmail.com>
2022-04-08 16:55:55 +0100

    Exercise -Upcoming Movies
    Added code for updateAccount

--- src/movies/routes/index.js
+++ src/movies/routes/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
-  import MoviesController from '../controllers';
+import MoviesController from '../controllers';
 
-  const createMoviesRouter = (dependencies) => {
-      const router = express.Router();
-      // load controllers with dependencies
-      const moviesController = MoviesController(dependencies);
+const createMoviesRouter = (dependencies) => {
+    const router = express.Router();
+    // load controllers with dependencies
+    const moviesController = MoviesController(dependencies);
 
-      router.route('/:id')
-          .get(moviesController.getMovie);
+    router.route('/:id')
+        .get(moviesController.getMovie);
 
-      router.route('/')
-          .get(moviesController.find);
+    router.route('/')
+        .get(moviesController.find);
 
-      return router;
-  };
-  export default createMoviesRouter;
+    router.route('/:id')
+        .post(moviesController.updateAccount);
+
+    router.route('/upcoming')
+        .get(moviesController.getUpcomingMovies);
+
+    return router;
+};
+export default createMoviesRouter;
--- src/movies/services/index.js
+++ src/movies/services/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import Account from '../entities/Accounts';
+
 
 export default {
     getMovie: async (movieId) => {
@@ -13,5 +15,17 @@ export default {
           );
           return response.data;
     },
+
+    updateAccount: async  (id, firstName, lastName, email, password, {accountsRepository}) => {
+      const account = new Account(undefined, firstName, lastName, email, password);
+      return accountsRepository.merge(account);
+    },
+
+    findUpcoming: async () => {
+      const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false`
+        );
+        return response.data;
+  }
 
   };
